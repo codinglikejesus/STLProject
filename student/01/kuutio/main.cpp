c78a97610bf73b8b7a36529072046b3c1b9b4a60
Luukas Lusetti <lusetti@savannihaikara.lin.tut.fi>
2018-01-11 18:55:51 +0200

    Väärä tiedosto korjattu oikealla

--- student/01/kuutio/main.cpp
+++ student/01/kuutio/main.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int number = 0;
+    int cube = 0;
+    cout << "Enter a number: " << endl;
+    cin >> number;
+    cube = number * number * number;
+    if(cube / number / number == number){
+        cout << "The cube of " << number << " is " << cube << "." << endl;
+    }
+    else{
+        cout << "Error! The cube of " << number << " is not " << cube << "." << endl;
+    }
+
 }
