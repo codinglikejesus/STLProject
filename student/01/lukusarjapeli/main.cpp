918f59972bda2863a779c4690b6bdbaab3378a1d
Luukas Lusetti <lusetti@savannihaikara.lin.tut.fi>
2018-01-11 18:13:29 +0200

    Ensimmainen osa tehtavasta

--- student/01/lukusarjapeli/main.cpp
+++ student/01/lukusarjapeli/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
-
+using namespace std;
 int main()
+
 {
-    std::cout << "How many numbers would you like to have? ";
+    cout << "How many numbers would you like to have? ";
+    int luku = 0;
+    cin >>luku;
+    int i= 0;
+    while( luku > i){
+        cout << i + 1 << endl;
+        i++;
+
+    }
 }
