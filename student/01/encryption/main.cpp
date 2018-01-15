4e3a76c9cc43317f20c02840daba8c1088b1668c
Luukas Lusetti <lusetti@nokihaikara.lin.tut.fi>
2018-01-15 20:35:13 +0200

    Puuttuu vielä salauksen muodostaminen

--- student/01/encryption/main.cpp
+++ student/01/encryption/main.cpp
@@ -2,15 +2,20 @@
 #include <cctype>
 
 using namespace std;
+string encrypter(){
+    for(char kirjain = 'a'; kirjain < 'z'; kirjain ++)
+
+}
 
 int main()
 {
+    string encrypted_text;
     string password;
     cout << "Enter the encryption key: ";
     cin >> password;
     if(password.length() != 26){
         cout << "Error! The encryption key must contain 26 characters.";
-        return 0;
+        EXIT_FAILURE;
     }
     for(int i = 0; i < 26; i++){
         if(islower(password.at(i))){
@@ -18,7 +23,26 @@ int main()
         }
         else{
             cout << "Error! The encryption key must contain only lower case characters.";
+            EXIT_FAILURE;
         }
 
     }
+    for( char kirjain = 'a'; kirjain < 'z'; ++kirjain){
+        if( password.find(kirjain) == string::npos){
+            cout << "Error! The encryption key must contain all alphabets a-z";
+            EXIT_FAILURE;
+        }
+    }
+    cout << "Enter the text to be encrypted: ";
+    cin >> encrypted_text;
+    for(int i = 0; i < encrypted_text.length(); i++){
+        if(islower(password.at(i))){
+
+        }
+        else{
+            cout << "Error! The encryption key must contain only lower case characters.";
+            EXIT_FAILURE;
+        }
+    }
 }
+
