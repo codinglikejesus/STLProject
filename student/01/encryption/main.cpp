65d0bdf63edbc670fde3735ea1fb03dde6180ff7
Luukas Lusetti <lusetti@nokihaikara.lin.tut.fi>
2018-01-16 13:17:10 +0200

    Lisatty salatun viestin luominen ja tulostus

--- student/01/encryption/main.cpp
+++ student/01/encryption/main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include <cctype>
+#include <string>
+#include <algorithm>
 
 using namespace std;
-string encrypter(){
-    for(char kirjain = 'a'; kirjain < 'z'; kirjain ++)
+string encrypter(string password, string text){
+    int i = 0;
+    string encrypted_text;
+    string original_text;
+    original_text = text;
+    string:: size_type pituus=0;
+    pituus = text.length();
+    char letter;
+    letter = password.at(i);
+    for(int j = 0; j < pituus; j++){
+        i = 0;
+        for(char kirjain = 'a'; kirjain < 'z'; kirjain ++){
+            if(text.string::at(j) == kirjain){
+                encrypted_text += password.string::at(i);
+            }
+            i++;
+
+        }
+
+    }
+    return encrypted_text;
 
 }
 
 int main()
 {
-    string encrypted_text;
+    string text;
     string password;
     cout << "Enter the encryption key: ";
     cin >> password;
@@ -34,9 +55,9 @@ int main()
         }
     }
     cout << "Enter the text to be encrypted: ";
-    cin >> encrypted_text;
-    for(int i = 0; i < encrypted_text.length(); i++){
-        if(islower(password.at(i))){
+    cin >> text;
+    for(int i = 0; i < text.length(); i++){
+        if(islower(text.at(i))){
 
         }
         else{
@@ -44,5 +65,7 @@ int main()
             EXIT_FAILURE;
         }
     }
+    string encrypted_text = encrypter(password, text);
+    cout << "Encrypted text: " << encrypted_text;
 }
 
