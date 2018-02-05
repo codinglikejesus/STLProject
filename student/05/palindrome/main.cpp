f520dd3f77cf9bbd05536116b715d278226cd6b9
Luukas Lusetti <lusetti@piispahaikara.lin.tut.fi>
2018-02-05 14:42:40 +0200

    ensimmainen versio

---
+++ student/05/palindrome/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+bool palindrome_recursive(std::string word, int begin, int ending){
+    if (word.at(begin) == word.at(ending)){
+        if ( begin == ending)
+            return true;
+        begin++;
+        ending--;
+        palindrome_recursive(word, begin, ending);
+    }
+    else
+        return false;
+}
+
+int main()
+{
+    int begin = 0;
+    std::cout << "Enter a word: ";
+    std::string word;
+    std::cin >> word;
+    int ending = word.size() - 1;
+
+    if(palindrome_recursive(word, begin, ending)){
+        std::cout << word << " is a palindrome" << std::endl;
+    } else {
+        std::cout << word << " is not a palindrome" << std::endl;
+    }
+}
